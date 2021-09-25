7c4fe2bc1b6542bd61fed4232cd12086cb599d9d
unknown <rajay230401@gmail.com>
2021-09-25 13:32:57 +0530

    Hello

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000);
+
+app.use(express.static('public'));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile('./views/index.html',{root:__dirname});
+});
+app.get('/about',(req,res)=>{
+    res.sendFile('./views/about.html',{root:__dirname})
+});
+app.get('/education',(req,res)=>{
+    res.sendFile('./views/education.html',{root:__dirname})
+});
+app.get('/accomplishments',(req,res)=>{
+    res.sendFile('./views/accomplishments.html',{root:__dirname})
+});
+app.get('/projects',(req,res)=>{
+    res.sendFile('./views/projects.html',{root:__dirname})
+});
+app.get('/gallery',(req,res)=>{
+    res.sendFile('./views/gallery.html',{root:__dirname})
+});
+
